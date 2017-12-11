f8bcfe4f223d7d4e63874f0b752aaa6bca237ef0
“CharlotteMD” <charlottemdavies@hotmail.co.uk>
2017-12-11 17:13:35 +0000

    missed commits: game logic complete, MVP, refactoring begun, begin of win/lose sequence animation, learnt about github - duh

--- js/script.js
+++ js/script.js
@@ -6,54 +6,89 @@ $(function(){
   const $cctvEyes = $('.cctv #eyes');
   const $cctvNose = $('.cctv #nose');
   const $cctvMouth = $('.cctv #mouth');
+  const $cctvDiv = $('.cctv');
+
+  let randomNumberEye = null;
+  let randomNumberNose = null;
+  let randomNumberMouth = null;
+
+  const $selectEyes = $('.selectors #eyes-select');
+  const $selectNose = $('.selectors #nose-select');
+  const $selectMouth = $('.selectors #mouth-select');
+  const $mugEyes = $('#mugshoteyes');
+  const $mugNose = $('#mugshotnose');
+  const $mugMouth = $('#mugshotmouth');
+
+  const $submit = $('.submit');
+
+  let compSetEyes = null;
+  let compSetNose = null;
+  let compSetMouth = null;
+
 
   $startButton.on('click', () => {
-    const randomNumberEye = Math.ceil(Math.random()*picsCount);
-    $cctvEyes.attr('src', `./images/faces/eyes/eyes-${randomNumberEye}.png`);
-    console.log(randomNumberEye);
+    randomNumberEye = Math.ceil(Math.random()*picsCount);
+    compSetEyes = ('eyes-' + randomNumberEye);
+    $cctvEyes.attr('src',`./images/faces/eyes/eyes-${randomNumberEye}.png`);
 
-    const randomNumberNose = Math.ceil(Math.random()*picsCount);
+    randomNumberNose = Math.ceil(Math.random()*picsCount);
+    compSetNose = ('nose-' + randomNumberNose);
     $cctvNose.attr('src', `./images/faces/nose/nose-${randomNumberNose}.png`);
-    console.log(randomNumberNose);
 
-    const randomNumberMouth = Math.ceil(Math.random()*picsCount);
-    $cctvMouth.attr('src', `./images/faces/mouth/mouth-${randomNumberMouth}.png`);
-    console.log(randomNumberMouth);
+    randomNumberMouth = Math.ceil(Math.random()*picsCount);
+    compSetMouth = ('mouth-' + randomNumberMouth);
+    $cctvMouth.attr('src',`./images/faces/mouth/mouth-${randomNumberMouth}.png`);
 
     setTimeout(() => {
       $cctvEyes.attr('src', './images/other/static-eyes.png');
       $cctvNose.attr('src', './images/other/static-nose.png');
       $cctvMouth.attr('src', './images/other/static-mouth.png');
     }, 1000);
-    // go back to original images after ten seconds
+
+    console.log('Villain is ' + compSetEyes);
+    console.log('Villain is ' + compSetNose);
+    console.log('Villain is ' + compSetMouth);
+
   });
-
-  // When you select eyes, the matching eyes appears in mugshot div in the eyes section
-
-  const $selectEyes = $('.selectors #eyes');
-  const $selectNose = $('.selectors #nose');
-  const $selectMouth = $('.selectors #mouth');
-  const $mugEyes = $('#mugshoteyes');
-  const $mugNose = $('#mugshotnose');
-  const $mugMouth = $('#mugshotmouth');
 
   $selectEyes.on('change', (event) => {
     const eyeNumber = (event.target.value);
-    console.log(eyeNumber);
+    console.log('Player chose ' + eyeNumber);
     $mugEyes.attr('src', `./images/faces/eyes/${eyeNumber}.png`);
   });
 
   $selectNose.on('change', (event) => {
     const noseNumber = (event.target.value);
-    console.log(noseNumber);
+    console.log('Player chose ' + noseNumber);
     $mugNose.attr('src', `./images/faces/nose/${noseNumber}.png`);
   });
 
   $selectMouth.on('change', (event) => {
     const mouthNumber = (event.target.value);
-    console.log(mouthNumber);
+    console.log('Player chose ' + mouthNumber);
     $mugMouth.attr('src', `./images/faces/mouth/${mouthNumber}.png`);
   });
 
+  $submit.on('click', (event) => {
+    event.preventDefault();
+    const selectedEyes = $('#eyes-select').val();
+    const selectedNose = $('#nose-select').val();
+    const selectedMouth = $('#mouth-select').val();
+    $cctvEyes.attr('src',`./images/faces/eyes/eyes-${randomNumberEye}.png`);
+    $cctvNose.attr('src', `./images/faces/nose/nose-${randomNumberNose}.png`);
+    $cctvMouth.attr('src',`./images/faces/mouth/mouth-${randomNumberMouth}.png`);
+
+
+    if ((selectedEyes === compSetEyes) && (selectedNose === compSetNose) && (selectedMouth === compSetMouth)) {
+      console.log('You win!');
+      // winSequence() => {
+      //   ./images/other/cell-bars.png
+
+    } else {
+      console.log('You lose!');
+      $cctvDiv.effect('shake', {direction: 'up', times: 4, distance: 10}, 1000 );
+      // div shakes & face disappers back to mugshot background, possibly add sound effect
+    }
+  });
 
 });
